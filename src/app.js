9314c8e1d8085a5f7ccb54253f0f09188fc7fe3c
Serge <linserga@gmail.com>
2022-07-23 19:00:27 +0300

    Added FiveDayForecast via API

--- src/app.js
+++ src/app.js
@@ -16,44 +16,52 @@ let form = document.querySelector("#search");
 let button = document.querySelector("#location");
 let apiKey = "d37ed2899e060781baede29f71370db4";
 
-function displayWeeklyForecast(day) {
-  let week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  let forecastHTML = `
-  <div class="col">
-  <div class="weather-forecast-date">${day}</div>
-  <img
-    src="https://openweathermap.org/img/wn/10d.png"
-    alt=""
-    width="42"
-  />
-  <div class="weather-forecast-temperatures">
-    <span class="weather-forecast-temperature-max">32°</span
-    ><span class="weather-forecast-temperature-min">24°</span>
-  </div>
-  `;
+function displayWeeklyForecast(response) {
+  let a = [];
+  let midnight = response.data.list.findIndex((element) =>
+    element.dt_txt.includes("00:00:00")
+  );
+  let noon = midnight + 4;
+  for (let i = noon; i < response.data.list.length; i += 8) {
+    a.push(response.data.list[i]);
+  }
 
-  week.forEach((day) => {
+  a.forEach((day, index) => {
+    let date = formatDate(day.dt);
+    let forecastHTML = `
+      <div class="col">
+      <div class="weather-forecast-date">${date.day}</div>
+      <img
+        src="https://openweathermap.org/img/wn/${day.weather[0].icon}.png"
+        alt=""
+        width="42"
+      />
+      <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max">${Math.round(
+          day.main.temp_max
+        )}°</span
+        ><span class="weather-forecast-temperature-min">${Math.round(
+          day.main.temp_min
+        )}°</span>
+      </div>
+      `;
     weeklyForecast.innerHTML += forecastHTML;
   });
 }
 
 function formatDate(timestamp) {
-  let week = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+  let week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   let date = new Date(timestamp * 1000);
   let hours = date.getHours();
   hours < 10 ? `0${hours}` : hours;
   let minutes = date.getMinutes();
   minutes < 10 ? `0${minutes}` : minutes;
   let day = week[date.getDay()];
-  return `Updated: ${day} ${hours}:${minutes}`;
+  return {
+    hours: hours,
+    minutes: minutes,
+    day: day,
+  };
 }
 
 function toFahrenheit(degree) {
@@ -64,19 +72,19 @@ function toFahrenheit(degree) {
 }
 
 function layout(response) {
-  weather = response.data;
-  city.innerHTML = weather.name;
-  humidity.innerHTML = weather.main.humidity;
-  wind.innerHTML = Math.round(weather.wind.speed);
-  temperature.innerHTML = Math.round(weather.main.temp);
-  tempInCelsius = Math.round(weather.main.temp);
-  atmosphere.innerHTML = weather.weather[0].description;
-  date.innerHTML = formatDate(weather.dt);
+  city.innerHTML = response.data.name;
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  tempInCelsius = Math.round(response.data.main.temp);
+  atmosphere.innerHTML = response.data.weather[0].description;
+  let formattedDate = formatDate(response.data.dt);
+  date.innerHTML = `Updated: ${formattedDate.day} ${formattedDate.hours}:${formattedDate.minutes}`;
   icon.setAttribute(
     "src",
-    `https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
   );
-  icon.setAttribute("alt", weather.weather[0].description);
+  icon.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchLocation() {
@@ -84,9 +92,19 @@ function searchLocation() {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
     let urlLocation = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-    axios.get(urlLocation).then((response) => {
-      layout(response);
-    });
+    let fiveDayCallUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    Promise.all([axios.get(urlLocation), axios.get(fiveDayCallUrl)]).then(
+      (results) => {
+        const mainLocation = results[0];
+        const forecastLocation = results[1];
+        layout(mainLocation);
+        displayWeeklyForecast(forecastLocation);
+      }
+    );
+    // axios.get(urlLocation).then((response) => {
+    //   layout(response);
+    // });
   });
 }
 
@@ -96,9 +114,16 @@ function search(cityInput) {
     .get(url)
     .then((response) => {
       layout(response);
+      let fiveDayCallUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${response.data.coord.lat}&lon=${response.data.coord.lon}&appid=${apiKey}&units=metric`;
+      axios
+        .get(fiveDayCallUrl)
+        .then((response) => {
+          displayWeeklyForecast(response);
+        })
+        .catch((error) => console.log(error));
     })
     .catch((error) => {
-      //msg.innerHTML = "We couldn't find this city. Try again";
+      console.log(error);
     });
 }
 
@@ -119,12 +144,13 @@ form.addEventListener("submit", (event) => {
   searchedCity = document.querySelector("#searchedCity").value;
   search(searchedCity);
   form.reset();
+  weeklyForecast.innerHTML = "";
 });
 
 button.addEventListener("click", (event) => {
   event.preventDefault();
   searchLocation();
+  weeklyForecast.innerHTML = "";
 });
 
 search("Paris");
-displayWeeklyForecast("Thu");
